c49caa82ce4d664e9f63f4331716aa61e8b89512
parK-dev <kirk.paradis@gmail.com>
2022-11-01 21:03:20 +0900

    Starter for a fresh Svelte + Typescript + TailwindCSS application

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  plugins: [],
+  theme: {
+    extend: {},
+  },
+  content: ["./index.html", "./src/**/*.{svelte,js,ts}"], // for unused CSS
+};
